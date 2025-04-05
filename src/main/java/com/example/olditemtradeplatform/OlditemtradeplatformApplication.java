0425860a651f0f8b682dd2767aa7daed4f1e7f39
JanooGwan <janoogwan39@gmail.com>
2025-04-05 19:39:20 +0900

    First Commit

---
+++ src/main/java/com/example/olditemtradeplatform/OlditemtradeplatformApplication.java
@@ -0,0 +1,13 @@
+package com.example.olditemtradeplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OlditemtradeplatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OlditemtradeplatformApplication.class, args);
+	}
+
+}
