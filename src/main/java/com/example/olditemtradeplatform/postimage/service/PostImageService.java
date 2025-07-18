6a19a70f897b775a0f25e74f9ea33ddd1b69c07a
JanooGwan <janoogwan39@gmail.com>
2025-07-18 19:46:21 +0900

    update : PostImageService - 예외 처리 관련 리팩토링

--- src/main/java/com/example/olditemtradeplatform/postimage/service/PostImageService.java
+++ src/main/java/com/example/olditemtradeplatform/postimage/service/PostImageService.java
@@ -1,13 +1,14 @@
 package com.example.olditemtradeplatform.postimage.service;
 
+import com.example.olditemtradeplatform.global.exception.CustomException;
 import com.example.olditemtradeplatform.post.domain.Post;
 import com.example.olditemtradeplatform.post.repository.PostRepository;
 import com.example.olditemtradeplatform.postimage.domain.PostImage;
 import com.example.olditemtradeplatform.postimage.domain.PostImageId;
 import com.example.olditemtradeplatform.postimage.dto.PostImageRequestDTO;
 import com.example.olditemtradeplatform.postimage.dto.PostImageResponseDTO;
+import com.example.olditemtradeplatform.postimage.exception.PostImageErrorCode;
 import com.example.olditemtradeplatform.postimage.repository.PostImageRepository;
-import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -22,13 +23,11 @@ public class PostImageService {
     private final FileStorageService fileStorageService;
 
     public PostImageResponseDTO saveImage(PostImageRequestDTO dto) {
-
         MultipartFile file = dto.getFile();
-
         String imageUrl = fileStorageService.save(file);
 
         Post post = postRepository.findById(dto.getPostId())
-                .orElseThrow(() -> new EntityNotFoundException("해당 게시글이 없습니다."));
+                .orElseThrow(() -> new CustomException(PostImageErrorCode.POST_NOT_FOUND));
 
         Long imageAt = postImageRepository.countByPost(post) + 1;
 
@@ -38,11 +37,10 @@ public class PostImageService {
         return PostImageResponseDTO.from(postImage);
     }
 
-
     @Transactional(readOnly = true)
     public PostImageResponseDTO findImage(PostImageId id) {
         PostImage postImage = postImageRepository.findById(id)
-                .orElseThrow(() -> new IllegalArgumentException("PostImage not found"));
+                .orElseThrow(() -> new CustomException(PostImageErrorCode.POST_IMAGE_NOT_FOUND));
         return PostImageResponseDTO.from(postImage);
     }
 
