9cdbdc686ae9c5c6cc74cdb839750ef499645415
JanooGwan <janoogwan39@gmail.com>
2025-07-18 15:56:21 +0900

    update : GlobalExceptionHandler 수정

--- src/main/java/com/example/olditemtradeplatform/global/GlobalExceptionHandler.java
+++ src/main/java/com/example/olditemtradeplatform/global/GlobalExceptionHandler.java
@@ -2,19 +2,26 @@ package com.example.olditemtradeplatform.global;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.util.HashMap;
 import java.util.Map;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler(RuntimeException.class)
-    public ResponseEntity<?> handleRuntimeException(RuntimeException e) {
-        return ResponseEntity
-                .status(HttpStatus.UNAUTHORIZED)
-                .body(Map.of("message", e.getMessage()));
-    }
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleValidationException(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
 
+        ex.getBindingResult().getFieldErrors().forEach(error -> {
+            String field = error.getField();
+            String message = error.getDefaultMessage();
+            errors.put(field, message);
+        });
+
+        return ResponseEntity.badRequest().body(errors);
+    }
 }
